4bbf3d9524f99385267933bfd46f9f793428bef3
Bernhard Meindl <bernhard.meindl@gmail.com>
2018-09-10 23:27:38 +0200

    document ExampleGDP

---
+++ R/vt_data.R
@@ -0,0 +1,21 @@
+#' ExampleGDP
+#'
+#' An example data.frame using GDP data to demonstrate the
+#' voronoiTree package
+#'
+#' @docType data
+#' @usage data(ExampleGDP)
+#' @format A data frame with 42 rows and 6 variables:
+#' \describe{
+#'   \item{h1}{Name of first-level (redundant)}
+#'   \item{h2}{Leaf names of second-level (continents)}
+#'   \item{h3}{Leaf names of third-level values (countries)}
+#'   \item{color}{colors in which the plot-regions will be filled}
+#'   \item{weight}{GDP values in percent of the overall total}
+#'   \item{codes}{short labels used for overlays in plotting}
+#' }
+#' @keywords datasets
+#' @examples 
+#' data("ExampleGDP")
+#' head(ExampleGDP)
+"ExampleGDP"
